863ab611f3f4f67c0f7302f92e22de80dbfbab96
Tyler Sanichara <Tsanichara67@gmail.com>
2025-03-23 14:23:49 -0400

    Added login functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 //@desc Register a user
 //@route POST /api/users/register
@@ -32,7 +33,6 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error("User data is not valid")
     }
 
-    res.json({message: "User registered"});
 });
 
 
@@ -40,7 +40,32 @@ const registerUser = asyncHandler(async (req, res) => {
 //@route POST /api/users/login
 //@access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: "Login user"});
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+
+    const user = await User.findOne({ email });
+    // compare password with hashedPassword
+    if(user && (await bcrypt.compare(password, user.password))){
+        const accessToken = jwt.sign({
+            user: {
+                username: user.username,
+                email: user.email,
+                id: user.id,
+            },
+        }, 
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: "1m" }
+    );
+        res.status(200).json({ accessToken });
+    } else {
+        res.status(401)
+        throw new Error("Invalid credentials");
+    }
+
 });
 
 
--- models/userModel.js
+++ models/userModel.js
@@ -11,7 +11,7 @@ const userSchema = mongoose.Schema({
         required: [true, "Please add an email address"],
         unique: [true, "Email address already taken"]
     },
-    username: {
+    password: {
         type: String,
         required: [true, "Please add a password"],
     },
